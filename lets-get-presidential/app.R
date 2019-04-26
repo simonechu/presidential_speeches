90d8ce2d0fe7fff7b69ca6b7135c16f05d61d4fb
Simone Chu <simonechu@dhcp-10-250-39-239.harvard.edu>
2019-04-26 16:41:07 -0400

    added over time plots

--- lets-get-presidential/app.R
+++ lets-get-presidential/app.R
@@ -12,7 +12,7 @@ library(shinythemes)
 library(ggplot2)
 library(tidyverse)
 
-# Importing plots that I've made!
+# Importing plots that I've made (of which there are many)! 
 
 i1993_topten <- read_rds("~/Desktop/presidential_speeches/lets-get-presidential/i1993_topten.rds")
 i2001_topten <- read_rds("~/Desktop/presidential_speeches/lets-get-presidential/i2001_topten.rds")
@@ -26,6 +26,17 @@ sotu2015_topten <- read_rds("~/Desktop/presidential_speeches/lets-get-presidenti
 sotu2016_topten <- read_rds("~/Desktop/presidential_speeches/lets-get-presidential/sotu2016_topten.rds")
 sotu2018_topten <- read_rds("~/Desktop/presidential_speeches/lets-get-presidential/sotu2018_topten.rds")
 sotu2019_topten <- read_rds("~/Desktop/presidential_speeches/lets-get-presidential/sotu2019_topten.rds")
+i1993_ot <- read_rds("~/Desktop/presidential_speeches/lets-get-presidential/i1993_ot.rds")
+i2001_ot <- read_rds("~/Desktop/presidential_speeches/lets-get-presidential/i2001_ot.rds")
+i2009_ot <- read_rds("~/Desktop/presidential_speeches/lets-get-presidential/i2009_ot.rds")
+i2017_ot <- read_rds("~/Desktop/presidential_speeches/lets-get-presidential/i2017_ot.rds")
+pressobama_ot <- read_rds("~/Desktop/presidential_speeches/lets-get-presidential/pressobama_ot.rds")
+presstrump_ot <- read_rds("~/Desktop/presidential_speeches/lets-get-presidential/presstrump_ot.rds")
+sotu2014_ot <- read_rds("~/Desktop/presidential_speeches/lets-get-presidential/sotu2014_ot.rds")
+sotu2015_ot <- read_rds("~/Desktop/presidential_speeches/lets-get-presidential/sotu2015_ot.rds")
+sotu2016_ot <- read_rds("~/Desktop/presidential_speeches/lets-get-presidential/sotu2016_ot.rds")
+sotu2018_ot <- read_rds("~/Desktop/presidential_speeches/lets-get-presidential/sotu2018_ot.rds")
+sotu2019_ot <- read_rds("~/Desktop/presidential_speeches/lets-get-presidential/sotu2019_ot.rds")
 
 # Define UI for application
 ui <- fluidPage(theme = shinytheme("lumen"),
@@ -43,7 +54,7 @@ ui <- fluidPage(theme = shinytheme("lumen"),
                                                      "Bush, 2001", "Clinton, 1993"),
                                          multiple = FALSE, 
                                          selected = "Trump, 2017")), 
-                mainPanel(plotOutput("i_topten")))),
+                mainPanel(plotOutput("i_topten"), plotOutput("i_ot")))),
      
      tabPanel("State of the Union",
               
@@ -56,13 +67,18 @@ ui <- fluidPage(theme = shinytheme("lumen"),
                                                      "Obama, 2014"),
                                          multiple = FALSE, 
                                          selected = "Trump, 2019")), 
-                mainPanel(plotOutput("sotu_topten")))),
+                mainPanel(plotOutput("sotu_topten"), plotOutput("sotu_ot")))),
      
      tabPanel("Press Relations",
               
               h1("An Analysis of Presidential Press Relations"),
               mainPanel(
-                   plotOutput("pressconfs"),
+                h3("President Obama's Press Conference - Jan. 18, 2017"),
+                plotOutput("ot_bo"),
+                h3("President Trump's Press Conference - Feb. 16, 2017"), 
+                plotOutput("ot_dt"), 
+                
+                plotOutput("pressconfs"),
                    align = "center")),
      
      tabPanel("About", htmlOutput("message"))
@@ -91,6 +107,24 @@ server <- function(input, output) {
     }
   }) 
   
+  output$i_ot <- renderPlot({
+    if (input$ia == "Trump, 2017") {
+      i2017_ot
+    }
+    
+    else if (input$ia == "Obama, 2009") {
+      i2009_ot
+    }
+    
+    else if (input$ia == "Bush, 2001") {
+      i2001_ot
+    }
+    
+    else if (input$ia == "Clinton, 1993") {
+      i1993_ot
+    }
+  })
+  
   output$sotu_topten <- renderPlot({
     if (input$sotu == "Trump, 2019") {
       sotu2019_topten
@@ -113,6 +147,28 @@ server <- function(input, output) {
     }
   })
   
+  output$sotu_ot <- renderPlot({
+    if (input$sotu == "Trump, 2019") {
+      sotu2019_ot
+    }
+    
+    else if (input$sotu == "Trump, 2018") {
+      sotu2018_ot
+    }
+    
+    else if (input$sotu == "Obama, 2016") {
+      sotu2016_ot
+    }
+    
+    else if (input$sotu == "Obama, 2015") {
+      sotu2015_ot
+    }
+    
+    else if (input$sotu == "Obama, 2014") {
+      sotu2014_ot
+    }
+  })
+  
   output$pressconfs <- renderPlot({
     
     press_plot %>%
@@ -124,6 +180,18 @@ server <- function(input, output) {
       labs(title = "Number of Press Conferences Held in First Year of Presidency",
            fill = "Party") + 
       theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  })
+  
+  output$ot_bo <- renderPlot({
+    
+    pressobama_ot
+    
+  })
+  
+  output$ot_dt <- renderPlot({
+    
+    presstrump_ot
+    
   })
   
   output$message <- renderUI({
